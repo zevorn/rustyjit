ef0a307043bdbbc33a639fc45d26e0132d078752
Chao Liu <chao.liu@yeah.net>
2024-10-07 21:33:34 +0800

    Supports the latest version of Rust compilation and eliminates compilation warnings

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,85 @@
-extern crate libc;
-
 use std::mem;
 use std::ops::{Index, IndexMut};
 
-extern {
-    fn memset(s: *mut libc::c_void, c: libc::uint32_t, n: libc::size_t) -> *mut libc::c_void;
-}
-
+// 定义页面大小常量
 const PAGE_SIZE: usize = 4096;
 
+/// JIT内存管理结构
 struct JitMemory {
-    contents : *mut u8
+    contents: *mut u8,
 }
 
 impl JitMemory {
+    /// 创建新的 JitMemory 实例
+    /// 
+    /// # 参数
+    /// - `num_pages`: 需要分配的页面数量
+    /// 
+    /// # 返回
+    /// - `JitMemory`: 新创建的 JitMemory 实例
     fn new(num_pages: usize) -> JitMemory {
-        let contents : *mut u8;
+        let contents: *mut u8;
         unsafe {
-            let size = num_pages * PAGE_SIZE;
-            let mut _contents : *mut libc::c_void = mem::uninitialized();
+            let size: usize = num_pages * PAGE_SIZE;
+            let mut _contents: *mut libc::c_void =
+                mem::MaybeUninit::<libc::c_void>::uninit().as_mut_ptr();
+            // 分配内存并对齐到页面大小
             libc::posix_memalign(&mut _contents, PAGE_SIZE, size);
-            libc::mprotect(_contents, size, libc::PROT_EXEC | libc::PROT_READ | libc::PROT_WRITE);
+            // 设置内存权限为可执行、可读、可写
+            libc::mprotect(
+                _contents,
+                size,
+                libc::PROT_EXEC | libc::PROT_READ | libc::PROT_WRITE,
+            );
 
-            memset(_contents, 0xc3, size);  // for now, prepopulate with 'RET'
+            // 初始化内存内容为 'RET' 指令
+            libc::memset(_contents, 0xc3, size);
 
             contents = mem::transmute(_contents);
         }
 
-        JitMemory { contents: contents }        
+        JitMemory { contents: contents }
     }
 }
 
+// 实现 Index trait，允许通过索引访问内存
 impl Index<usize> for JitMemory {
-    type Output = u8;
+    type Output = u8; // 声明了 Index<usize> trait 的关联类型 Output 为 u8 类型
 
     fn index(&self, _index: usize) -> &u8 {
-        unsafe {&*self.contents.offset(_index as isize) }
+        unsafe { &*self.contents.offset(_index as isize) }
     }
 }
 
+// 实现 IndexMut trait，允许通过索引修改内存
 impl IndexMut<usize> for JitMemory {
     fn index_mut(&mut self, _index: usize) -> &mut u8 {
-        unsafe {&mut *self.contents.offset(_index as isize) }
+        unsafe { &mut *self.contents.offset(_index as isize) }
     }
 }
 
-fn run_jit() -> (fn() -> i64) {
-    let mut jit : JitMemory = JitMemory::new(1);
+/// 编译并返回一个函数，该函数在 JIT 内存中生成指定的机器码
+/// 
+/// # 返回
+/// - `fn() -> i64`: 一个在 JIT 内存中生成的函数，返回一个 i64 值
+fn run_jit() -> fn() -> i64 {
+    let mut jit: JitMemory = JitMemory::new(1);
 
-    jit[0] = 0x48;  // mov RAX, 0x3
+    // 在 JIT 内存中写入机器码，生成一个返回 3 的函数
+    jit[0] = 0x48; // mov RAX, 0x30
     jit[1] = 0xc7;
     jit[2] = 0xc0;
-    jit[3] = 0x3;
+    jit[3] = 0x30;
     jit[4] = 0;
     jit[5] = 0;
     jit[6] = 0;
 
+    // 将内存指针转换为函数指针并返回
     unsafe { mem::transmute(jit.contents) }
 }
 
 fn main() {
-    let fun = run_jit();
-    println!("{}", fun());
+    // 生成并调用 JIT 编译的函数，并打印结果
+    let fun: fn() -> i64 = run_jit();
+    println!("{:#x}", fun());
 }
